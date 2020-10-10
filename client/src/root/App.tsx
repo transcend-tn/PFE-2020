207b3579c21513bd8c4b7d351759d3ada01e4597
Marwen Arfa <marwen.arfa@gmail.com>
2020-10-10 17:48:08 +0200

    setup react-router

--- client/src/root/App.tsx
+++ client/src/root/App.tsx
@@ -1,38 +1,28 @@
 import React from 'react';
-import Card from 'react-bootstrap/esm/Card';
-import Button from 'react-bootstrap/esm/Button';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route} from "react-router-dom";
 
-import { useStoreActions, useStoreState } from '../hooks/store.hooks';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 import './styles/index.scss'
 
 function App() {
-  const fetchUsers = useStoreActions(state => state.users.fetchUsers);
-  const users = useStoreState(state => state.users.items);
 
   return (
-    <div className="App">
-      <Button variant="danger" className="mt-5 ml-5" onClick={() => fetchUsers()} >
-        GET
-      </Button>
-      <hr/>
-      <div className="d-flex flex-wrap justify-content-center">
-        {
-          users.map((user: any) => {
-            return (
-              <Card className="mr-3 mb-3 w-25" key={`user-${user.id}`}>
-                <Card.Body>
-                  <Card.Title>{user.username}</Card.Title>
-                  <Card.Text>{user.email}</Card.Text>
-                  <Button variant="primary" className="mt-3">Detail</Button>
-                </Card.Body>
-              </Card>
-            );
-          })
-        }
-      </div>
-    </div>
-  );
+    <Router>
+      <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+    </Router>
+  )
 }
 
 export default App;
