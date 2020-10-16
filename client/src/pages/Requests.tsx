94faa12b0b31584b695bee94a8a6d567bc1c6664
Arfaoui-khouloud <72348339+Arfaoui-khouloud@users.noreply.github.com>
2020-10-16 23:06:35 +0200

    CollaborationRequest
    * CollaborationRequest

---
+++ client/src/pages/Requests.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CollaborationRequest from "../components/CollaborationRequest";
+
+
+
+function Requests() {
+  return <CollaborationRequest username="Username 1" document="Document 1" />;
+}
+
+export default Requests;
