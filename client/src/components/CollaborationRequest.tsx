5af10dc9d29cb5e07734f0e33ff3c4ad5b6d238d
Marwen Arfa <marwen.arfa@gmail.com>
2020-10-16 22:56:19 +0200

    fix: collaboration request

--- client/src/components/CollaborationRequest.tsx
+++ client/src/components/CollaborationRequest.tsx
@@ -3,7 +3,14 @@ import Media from "react-bootstrap/esm/Media";
 import Button from "react-bootstrap/esm/Button";
 import Image from "react-bootstrap/esm/Image";
 
-function CollaborationRequest(props: any) {
+export interface CollaborationRequestProps {
+  /** username prop documentation */
+  username: string;
+  /** document prop documentation */
+  document: string;
+}
+
+function CollaborationRequest(props: CollaborationRequestProps) {
   function HandlerAccepter() {
     console.log(
       `${props.username} " a été ajouté au "${props.document}" en tant que collaborateur"`
@@ -14,31 +21,29 @@ function CollaborationRequest(props: any) {
     console.log(`la demande de "${props.username}" a été refusée"`);
   }
   return (
-    <div>
-        <Media>
-          <Image
-            src="https://picsum.photos/200"
-            rounded
-            width={68}
-            height={68}
-            className="mr-2"
-          />
-          <Media.Body>
-            <div className="description">
-              <h5>{props.username}</h5>
-              <p>{props.document}</p>
-            </div>
-            <div className="actions">
-              <Button variant="danger" type="submit" onClick={HandlerRefuse}>
-                Refuser
-              </Button>
-              <Button variant="success" type="submit" onClick={HandlerAccepter}>
-                Accepter
-              </Button>
-            </div>
-          </Media.Body>
-        </Media>
-    </div>
+    <Media>
+      <Image
+        src="https://picsum.photos/200"
+        rounded
+        width={68}
+        height={68}
+        className="mr-2"
+      />
+      <Media.Body className="d-flex justify-content-between align-items-center">
+        <div className="description">
+          <h5>{props.username}</h5>
+          <p>{props.document}</p>
+        </div>
+        <div className="actions">
+          <Button variant="danger" type="submit" onClick={HandlerRefuse}>
+            Refuser
+          </Button>
+          <Button variant="success" type="submit" onClick={HandlerAccepter}>
+            Accepter
+          </Button>
+        </div>
+      </Media.Body>
+    </Media>
   );
 }
 
