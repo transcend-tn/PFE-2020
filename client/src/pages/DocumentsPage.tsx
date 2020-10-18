5ac4ad7c2f7f3b67af90d5edb2867c2bfa492bb8
Maroaun <ben.elheni.marouan@arfacod.com>
2020-10-19 01:17:21 +0200

    fix: Documents page

--- client/src/pages/DocumentsPage.tsx
+++ client/src/pages/DocumentsPage.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Favoris from "../components/FavorisCard";
+import ProfileCard from "../components/ProfileCard";
 
+const FAVORIS: number[] = [1, 2];
 
-function DocumentsPage() {
+function DocumentPage() {
   return (
-    <div>
-      Mes Documents
-    </div>
+    <Row>
+      <Col>
+        <ProfileCard followers={15} username="Username" following={23} />
+      </Col>
+      <Col>
+        <div>
+          <h2 className="mb-3">Mes Documents</h2>
+          <>
+           {FAVORIS.map(value => {
+             return (
+               <Favoris key={`favoris-${value}`} document='document 1' timeEdit="12/04/2019" />
+             )
+           })}
+          </>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
-export default DocumentsPage;
+export default DocumentPage;
