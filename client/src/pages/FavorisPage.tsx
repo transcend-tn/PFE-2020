227c8d9f13ad7686b216334c95bec05b6c06bf90
Marwen Arfa <marwen.arfa@gmail.com>
2020-10-18 20:18:45 +0200

    fix: design favoris & requests

--- client/src/pages/FavorisPage.tsx
+++ client/src/pages/FavorisPage.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import Favoris from "../components/Favoris";
+import ProfileCard from "../components/ProfileCard";
+
+const FAVORIS: number[] = [1, 2];
 
 function FavorisPage() {
   return (
-    <>
-      <Favoris document='document 1' timeEdit="12/04/2019" />
-    </>
+    <Row>
+      <Col>
+        <ProfileCard followers={15} username="Username" following={23} />
+      </Col>
+      <Col>
+        <div>
+          <h2 className="mb-3">Mes Favoris</h2>
+          <>
+           {FAVORIS.map(value => {
+             return (
+               <Favoris key={`favoris-${value}`} document='document 1' timeEdit="12/04/2019" />
+             )
+           })}
+          </>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
--- client/src/pages/Requests.tsx
+++ client/src/pages/Requests.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import CollaborationRequest from "../components/CollaborationRequest";
+import ProfileCard from "../components/ProfileCard";
 
-
+const REQUESTS: number[] = [1, 2];
 
 function Requests() {
-  return <CollaborationRequest username="Username 1" document="Document 1" />;
+  return (
+    <Row>
+      <Col>
+        <ProfileCard followers={15} username="Username" following={23} />
+      </Col>
+      <Col>
+        <div>
+          <h2 className="mb-3">Les Demandes</h2>
+          <>
+           {REQUESTS.map(value => {
+             return (
+               <CollaborationRequest key={`request-${value}`} username="Username 1" document="Document 1" />
+             )
+           })}
+          </>
+        </div>
+      </Col>
+    </Row>
+  );
 }
 
 export default Requests;
