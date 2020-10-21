58513b011f0cca75b8ab7160d89ae6b4a53b0ff9
Marwen Arfa <marwen.arfa@gmail.com>
2020-10-21 21:26:16 +0200

    fix: remove deprecated button

--- client/src/components/DocumentHeader.tsx
+++ client/src/components/DocumentHeader.tsx
@@ -10,13 +10,13 @@ export interface DocumentHeaderProps {
 
 function DocumentHeader(props: DocumentHeaderProps) {
   return (
-    <Card>
-      <Card.Title className="mt-3 ml-3 mb-1">{props.title}</Card.Title>
-      <Card.Text className="text-muted ml-3"> Créé à {props.dateCeation}</Card.Text>
-      <div className="d-flex flex-row-reverse m-3">
+    <Card className="p-3">
+      <Card.Title>{props.title}</Card.Title>
+      <Card.Text className="text-muted"> Créé {props.dateCeation}</Card.Text>
+      <div className="d-flex flex-row-reverse">
         <Link to="edit-document">
-          <Button variant="secondary" type="submit">
-            Edit
+          <Button variant="primary" type="submit">
+            Modifier
           </Button>
         </Link>
       </div>
