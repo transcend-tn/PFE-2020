1fe0608018e6333ef86d338db74e4c68b313c11f
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-21 09:43:23 +0100

    feat(api): getuser decorator
    create custom decorator GetUser() to retrieve user from request

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -2,11 +2,19 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { CreateUserDto } from './dto/createUser.dto';
+import { GetUser } from './get-user.decorator';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  getMe(@GetUser() user: User) {
+    return user;
+  }
 
   @Get()
   @UseGuards(AuthGuard())
