8eea04e26e354f063a912041c653ce26a792be4b
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-21 01:49:37 +0100

    feat(pages): react draft wysiwyg

--- client/src/pages/NewDocumentsPage.tsx
+++ client/src/pages/NewDocumentsPage.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import { convertFromRaw } from 'draft-js';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import Card from 'react-bootstrap/esm/Card';
 
-function NewDocumentPage() {
-  return <h5 className="mb-3">Create New Document</h5>;
+const content = {
+  entityMap: {},
+  blocks: [
+    {
+      key: '637gr',
+      text: 'Initialized from content state.',
+      type: 'unstyled',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+  ],
+};
+
+class NewDocumentPage extends Component {
+  constructor(props: any) {
+    super(props);
+    const contentState = convertFromRaw(content);
+    this.state = {
+      contentState,
+    };
+  }
+
+  onContentStateChange = (contentState: any) => {
+    this.setState({
+      contentState,
+    });
+  };
+
+  render() {
+    const { contentState }: any = this.state;
+    return (
+      <div>
+        <Card>
+          <Editor
+            wrapperClassName="mt-4 ml-4 mr-4 mb-4"
+            editorClassName="ml-4 mb-4"
+            editorStyle={{ maxHeight: '310px' }}
+            toolbar={{
+              options: [
+                'history',
+                'inline',
+                'blockType',
+                'fontSize',
+                'colorPicker',
+                'textAlign',
+                'list',
+                'link',
+                'emoji',
+              ],
+            }}
+            onContentStateChange={this.onContentStateChange}
+          />
+        </Card>
+        <div className="d-flex flex-row-reverse" style={{ width: 'auto' }}>
+          <Button
+            className="d-flex flex-row-reverse mt-4"
+            variant="secondary"
+            onClick={() => console.log(contentState.blocks)}
+          >
+            Post
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default NewDocumentPage;
