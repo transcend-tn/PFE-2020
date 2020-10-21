6509ee2a6afeba13471b6d230b19ecfe006e1417
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-21 04:32:03 +0100

    feat(pages): react draft wysiwyg

--- client/src/pages/NewDocumentsPage.tsx
+++ client/src/pages/NewDocumentsPage.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import { convertFromRaw } from 'draft-js';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import Card from 'react-bootstrap/esm/Card';
 
-function NewDocumentPage() {
-  return <h5 className="mb-3">Create New Document</h5>;
+const EDITOR_OPTIONS = [
+  'history',
+  'inline',
+  'blockType',
+  'fontSize',
+  'colorPicker',
+  'textAlign',
+  'list',
+  'link',
+  'emoji',
+];
+
+const content = {
+  entityMap: {},
+  blocks: [
+    {
+      key: '637gr',
+      text: 'Initialized from content state.',
+      type: 'unstyled',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+  ],
+};
+
+class NewDocumentPage extends Component {
+  constructor(props: any) {
+    super(props);
+    const contentState = convertFromRaw(content);
+    this.state = {
+      contentState,
+    };
+  }
+
+  onContentStateChange = (contentState: any) => {
+    this.setState({
+      contentState,
+    });
+  };
+
+  onSubmitContent = () => {
+    const { contentState }: any = this.state;
+    console.log('contentState: ', contentState);
+  };
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <div className="m-4">
+            <Button variant="primary" onClick={this.onSubmitContent}>
+              Enregistrer
+            </Button>
+          </div>
+          <Editor
+            wrapperClassName="m-4"
+            editorClassName="ml-4 mb-4"
+            toolbar={{
+              options: EDITOR_OPTIONS,
+            }}
+            onContentStateChange={this.onContentStateChange}
+          />
+        </Card>
+      </div>
+    );
+  }
 }
-
 export default NewDocumentPage;
