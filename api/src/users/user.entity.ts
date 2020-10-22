bde1b9e0ab664625774726a8a85f7c3670b6d046
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-23 00:44:50 +0100

    feat(api): set user default img

--- api/src/users/user.entity.ts
+++ api/src/users/user.entity.ts
@@ -25,6 +25,9 @@ export class User extends BaseEntity {
   @Column()
   salt: string;
 
+  @Column()
+  img: string;
+
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
     return hash === this.password;
