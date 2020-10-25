e181e5109fd107d31da5f8bb1aa4460e5d74bc02
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-25 18:27:20 +0100

    fix(client): proposition-details

--- packages/client/src/pages/EditDocumentPage.tsx
+++ packages/client/src/pages/EditDocumentPage.tsx
@@ -60,40 +60,40 @@ class EditDocumentPage extends Component {
       <>
         <div className="d-flex flex-row-reverse">
           <Link to="document">
-            <Button variant="primary" onClick={this.onSubmitContent}>
+            <Button className="mb-3" size="sm" variant="primary" onClick={this.onSubmitContent}>
               Proposer Modification
             </Button>
           </Link>
         </div>
-        <Tabs defaultActiveKey="Document" id="uncontrolled-tab">
-          <Tab eventKey="Document" title="Document" className="mt-5">
-            <Card className="card-header p-2 bg-white">
-              <Editor
-                wrapperClassName="m-4"
-                editorClassName="ml-4 mb-4"
-                toolbar={{
-                  options: EDITOR_OPTIONS,
-                }}
-                onContentStateChange={this.onContentStateChange}
-              />
-            </Card>
-          </Tab>
-          <Tab eventKey="PR" title="Propositions de Modifications" className="mt-5">
-            <Row>
-              <div className="card p-3" style={{ width: '50%' }}>
+        <div className="card p-3 min">
+          <Tabs defaultActiveKey="Document" id="uncontrolled-tab">
+            <Tab eventKey="Document" title="Document" className="mt-5">
+              <Card className="card-header p-2 bg-white">
+                <Editor
+                  wrapperClassName="m-4"
+                  editorClassName="ml-4 mb-4"
+                  toolbar={{
+                    options: EDITOR_OPTIONS,
+                  }}
+                  onContentStateChange={this.onContentStateChange}
+                />
+              </Card>
+            </Tab>
+            <Tab eventKey="PR" title="Propositions de Modifications" className="mt-5">
+              <Row>
                 <Col lg="6" className="mb-3">
-                  OLD VERSION TEXT HERE
+                  <div className="card p-3">OLD VERSION TEXT HERE</div>
                 </Col>
-              </div>
-              <Col lg="6">
-                <div className="card p-3 min">
-                  {/* TODO: show all lines instead of only first line */}
-                  {contentState.blocks ? contentState.blocks[0].text : ''}
-                </div>
-              </Col>
-            </Row>
-          </Tab>
-        </Tabs>
+                <Col lg="6">
+                  <div className="card p-3 min">
+                    {/* TODO: show all lines instead of only first line */}
+                    {contentState.blocks ? contentState.blocks[0].text : ''}
+                  </div>
+                </Col>
+              </Row>
+            </Tab>
+          </Tabs>
+        </div>
       </>
     );
   }
