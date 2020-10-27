eba9e596c59df0857f897e7ef51c862086bcb153
Arfaoui-khouloud <72348339+Arfaoui-khouloud@users.noreply.github.com>
2020-10-27 17:26:48 +0100

    feat: get document by id

--- packages/client/src/constants/uris.ts
+++ packages/client/src/constants/uris.ts
@@ -7,3 +7,4 @@ export const SIGN_UP = '/users';
 export const USER_BY_ID = (id: string) => `/users'/${id}`;
 
 export const DOCUMENT = '/document';
+export const DOCUMENT_BY_ID = (id: string) => `/document/${id}`;
