0a3cc2efbbbc7c06dc7317bf92d091d288b0ceda
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-27 12:33:20 +0100

    feat(api): user favorite relaions

---
+++ packages/api/src/favorite/favorite.entity.ts
@@ -0,0 +1,25 @@
+import { User } from '../users/user.entity';
+import {
+  Entity,
+  Unique,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity()
+export class Favorite extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  documentId: string;
+
+  @ManyToOne(type=>User, user=>user.favorites, {eager :false})
+  user:User;
+
+}
