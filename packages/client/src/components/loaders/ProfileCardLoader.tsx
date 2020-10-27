ce6e2b10304d51deef6a8c40dc28006e2258b66b
Marwen Arfa <marwen.arfa@gmail.com>
2020-10-27 20:08:37 +0100

    style: fix ProfileCardLoader

--- packages/client/src/components/loaders/ProfileCardLoader.tsx
+++ packages/client/src/components/loaders/ProfileCardLoader.tsx
@@ -5,8 +5,8 @@ const ProfileCardLoader = () => {
   return (
     <div className="text-center card-body bg-white border" style={{ height: 229 }}>
       <RoundShape color="#E0E0E0" style={{ width: 100, height: 100 }} className="ml-auto mr-auto" />
-      <TextRow color="#E0E0E0" style={{ width: 200, height: 20 }} className="mt-3 ml-auto mr-auto" />
-      <RectShape color="#E0E0E0" style={{ width: 80, height: 30 }} className="mt-3 ml-auto mr-auto" />
+      <TextRow color="#E0E0E0" style={{ width: 100, height: 20 }} className="mt-3 ml-auto mr-auto" />
+      <RectShape color="#E0E0E0" style={{ width: 150, height: 30 }} className="mt-3 ml-auto mr-auto" />
     </div>
   );
 };
