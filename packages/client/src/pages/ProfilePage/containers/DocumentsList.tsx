adcc0bb73a6d9d8b0037342c822e3edea45f5058
Arfaoui-khouloud <72348339+Arfaoui-khouloud@users.noreply.github.com>
2020-10-27 11:41:11 +0100

    feat: document-get-by-owner

--- packages/client/src/pages/ProfilePage/containers/DocumentsList.tsx
+++ packages/client/src/pages/ProfilePage/containers/DocumentsList.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactPlaceholder from 'react-placeholder';
 import { useQuery } from 'react-query';
 import Favoris from '../../../components/FavorisCard';
-import { documentgetAllQuery } from '../../../services/document.service';
+import {getDocumentByOwner } from '../../../services/document.service';
 
 function DocumentsList() {
-  const { isLoading, isError, data = [], error } = useQuery('documents:getall', documentgetAllQuery);
+  const { isLoading, isError, data = [], error } = useQuery('documents:getbyowner', getDocumentByOwner);
 
   if (isError) {
     return <span>Error: {error} !</span>;
