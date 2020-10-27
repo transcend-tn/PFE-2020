2edf148c1bd87e3e0b363aee6974b47363559c20
Arfaoui-khouloud <khouloudarfaoui234@gmail.com>
2020-10-27 20:18:58 +0100

    fix:document-update

--- packages/client/src/services/document.service.ts
+++ packages/client/src/services/document.service.ts
@@ -1,5 +1,6 @@
 import axios from '../config/axios';
-import { DOCUMENT, DOCUMENT_BY_ID } from './../constants/uris';
+import { DOCUMENT, DOCUMENTUPDATE, DOCUMENT_BY_ID } from './../constants/uris';
+import { DocumentUpdate } from '../../../common/src/document.interface';
 
 export const createDocumentMutation = async (payload: any): Promise<string> => {
   const { data } = await axios.post(DOCUMENT, payload);
@@ -22,7 +23,7 @@ export const deleteDocumentMutation = async (payload: any): Promise<string> => {
   const { data } = await axios.delete(DOCUMENT_BY_ID(payload));
   return data;
 };
-export const updateDocumentMutation = async (payload: any): Promise<string> => {
-  const { data } = await axios.put(DOCUMENT, payload);
+export const updateDocumentMutation = async (payload: DocumentUpdate): Promise<string> => {
+  const { data } = await axios.put(DOCUMENTUPDATE, payload);
   return data;
 };
