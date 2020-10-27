12a2f2be18fbf3d43a250d1f1eaedfa51a04d572
Arfaoui-khouloud <72348339+Arfaoui-khouloud@users.noreply.github.com>
2020-10-27 17:25:41 +0100

    feat: get user by id

--- packages/client/src/services/user.service.ts
+++ packages/client/src/services/user.service.ts
@@ -1,7 +1,7 @@
 import { UserCreate, UserLogin } from '@tr/common';
 
 import axios from '../config/axios';
-import { SIGN_IN, SIGN_UP } from '../constants/uris';
+import {SIGN_IN, SIGN_UP, USER_BY_ID } from '../constants/uris';
 
 export const signIn = async (payload: UserLogin): Promise<string> => {
   const { data } = await axios.post(SIGN_IN, payload);
@@ -10,5 +10,10 @@ export const signIn = async (payload: UserLogin): Promise<string> => {
 
 export const signUp = async (payload: UserCreate): Promise<void> => {
   const { data } = await axios.post(SIGN_UP, payload);
+  return data;
+};
+
+export const getUserById = async (payload: string): Promise<any> => {
+  const { data } = await axios.get( USER_BY_ID (payload));
   return data;
 };
