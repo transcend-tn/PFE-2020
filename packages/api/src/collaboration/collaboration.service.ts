e3938c608b8265f406d399e036aeae3d570283f6
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-28 16:05:50 +0100

    fix: model import

--- packages/api/src/collaboration/collaboration.service.ts
+++ packages/api/src/collaboration/collaboration.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Collaboration } from '../../dist/collaboration/collaboration.model';
+import { Collaboration } from './collaboration.model';
 
 @Injectable()
 export class CollaborationService {
