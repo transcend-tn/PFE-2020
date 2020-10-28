83b82c3af9c1180dac97fef8b276427d26cdc851
Arfaoui-khouloud <khouloudarfaoui234@gmail.com>
2020-10-28 14:45:20 +0100

    feat: favoris-add-favoris

--- packages/client/src/services/favorite.service.ts
+++ packages/client/src/services/favorite.service.ts
@@ -1,14 +1,12 @@
 import axios from '../config/axios';
 import { FAVORITE, FAVORITE_BY_ID } from '../constants/uris';
 
-
 export const addFavoriteMutation = async (payload: string): Promise<string> => {
-    const { data } = await axios.post(FAVORITE, payload);
-    return data;
-  };
+  const { data } = await axios.post(FAVORITE, payload);
+  return data;
+};
 
-  export const   removeFavoritetMutation = async (payload: string): Promise<string> => {
-    const { data } = await axios.delete(FAVORITE_BY_ID (payload));
-    return data;
-  };
-  
+export const removeFavoritetMutation = async (payload: string): Promise<string> => {
+  const { data } = await axios.delete(FAVORITE_BY_ID(payload));
+  return data;
+};
