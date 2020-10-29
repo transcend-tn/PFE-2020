0dab1569edf99b9c5014858f974c79a70fb756fe
Arfaoui-khouloud <khouloudarfaoui234@gmail.com>
2020-10-29 16:24:00 +0100

    feat: demande-modification

---
+++ packages/client/src/pages/DocumentPage/containers/TeamMembersListContainer.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import MemberCard from '../../../components/MemberCard';
+import { COLLABORATION_BY_ID } from '../../../constants/uris';
+
+
+function TeamMembersListContainer() {
+
+  const [pending, setPending] = useState(false);
+  const { id } = useParams<{ id: string }>();
+  const { isError, data = {}, error } = useQuery([id], COLLABORATION_BY_ID);
+
+  if (isError) {
+    return <span>Error: {error} !</span>;
+  }
+
+  return (
+
+      <>
+      <Card>
+        <Card.Title>
+          <h5 className="d-flex justify-content-center mt-4">Team Members</h5>
+        </Card.Title>
+        <Card.Body>
+          {data.map((msg: any, idx: number) => {
+            return <MemberCard img={msg.img} username={msg.username} key={`member-${idx}`} />;
+          })}
+          <hr />
+          <div className="text-center">
+            <Button variant="success" onClick={() => setPending(true)} disabled={pending}>
+              {pending ? 'Pending' : 'Join Team'}
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+   
+    </>
+  );
+}
+
+export default TeamMembersListContainer;
