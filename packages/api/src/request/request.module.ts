bc7b137d3f163af403352b704078f47c3c87534a
Wassim Arfa <wassim.arfa@gmail.com>
2020-10-29 16:02:21 +0100

    refactor(api): only team members can request

--- packages/api/src/request/request.module.ts
+++ packages/api/src/request/request.module.ts
@@ -4,11 +4,15 @@ import { RequestService } from './request.service';
 import { RequestSchema } from './request.model';
 import { UsersModule } from '../users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CollaborationSchema } from '../collaboration/collaboration.model';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forFeature([{ name: 'Request', schema: RequestSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Collaboration', schema: CollaborationSchema },
+    ]),
   ],
   controllers: [RequestController],
   providers: [RequestService]
--- packages/api/src/request/request.service.ts
+++ packages/api/src/request/request.service.ts
@@ -4,20 +4,31 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Request } from './Request.model';
 import { User } from '../users/user.entity';
 import { RequestCreate } from '@tr/common';
+import { Collaboration } from '../collaboration/collaboration.model';
+import { CollaborationService } from '../collaboration/collaboration.service';
+import { UnauthorizedException } from '@nestjs/common/exceptions/unauthorized.exception';
 
 @Injectable()
 export class RequestService {
   constructor(
     @InjectModel('Request') private readonly requestModel: Model<Request>,
+    @InjectModel('Collaboration')
+    private readonly collaborationModel: Model<Collaboration>,
   ) {}
 
   async createRequest(currentUser: User, id: string, newRequest: RequestCreate) {
-    let request = new this.requestModel();
+    const isMember = await new CollaborationService(this.collaborationModel).isMember(currentUser, id);
+    if (isMember)
+    { let request = new this.requestModel();
     request.documentId = id;
     request.userId = currentUser.id;
     request.title = newRequest.title;
     request.body = newRequest.body;
     return await request.save();
+
+    }else
+    throw new UnauthorizedException('You have to be a member of the team to create a request')
+   
   }
 
   async getRequest(id: string) {
