c01a0cd81f291dd7008c0d08a4449ed733cef658
Wassim Arfa <wassim.arfa@gmail.com>
2020-11-01 22:05:56 +0100

    refactor(api): add comment on request
    [CLIENT] : Need change comment on document uri
    /document/:id

--- packages/api/src/comment/comment.controller.ts
+++ packages/api/src/comment/comment.controller.ts
@@ -10,13 +10,23 @@ import { User } from '../users/user.entity';
 export class CommentController {
     constructor(private commentService: CommentService) {}
 
-    @Post(':id')
-    addComment(@Body() comment:CommentCreate, @GetUser() currentUser:User, @Param('id') id:string)
-    {return this.commentService.addComment(comment, currentUser, id);}
+    @Post('/document/:id')
+    documentComment(@Body() comment:CommentCreate, @GetUser() currentUser:User, @Param('id') id:string)
+    {return this.commentService.documentComment(comment, currentUser, id);}
 
     @Get('/document/:id')
     getCommentById(@Param('id') id: string) {
-      return this.commentService.getCommentById(id);
+      return this.commentService.getCommentByDocId(id);
     }
+
+    @Post('/request/:id')
+    requestComment(@Body() comment:CommentCreate, @GetUser() currentUser:User, @Param('id') id:string)
+    {return this.commentService.requestComment(comment, currentUser, id);}
+
+    @Get('/request/:id')
+    getCommentByReqId(@Param('id') id: string) {
+      return this.commentService.getCommentByReqId(id);
+    }
+
   
 }
