320ef56efcb3803dde3fe258a2d7c10ce4e8738b
Wassim Arfa <wassim.arfa@gmail.com>
2020-11-03 18:19:48 +0100

    refactor(api): comment

--- packages/api/src/comment/comment.module.ts
+++ packages/api/src/comment/comment.module.ts
@@ -4,10 +4,13 @@ import { CommentService } from './comment.service';
 import { UsersModule } from '../users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommentSchema } from './comment.model';
+import { UserRepository } from '../users/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UsersModule,
+    TypeOrmModule.forFeature([UserRepository]),
     MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
   ],
   controllers: [CommentController],
