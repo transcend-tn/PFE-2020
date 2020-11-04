5bccf67934cb4a21d5222ffc11261a96e664d0af
Wassim Arfa <wassim.arfa@gmail.com>
2020-11-04 23:38:52 +0100

    refactor: edit doc page

--- packages/client/src/components/DocumentHeader.tsx
+++ packages/client/src/components/DocumentHeader.tsx
@@ -1,11 +1,8 @@
-import { format, formatRelative, subDays } from 'date-fns';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/esm/Card';
-import { useQuery } from 'react-query';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { DOCUMENT_EDIT } from '../constants/uris';
-import { getDocumentById } from '../services/document.service';
 
 export interface DocumentHeaderProps {
   title: string;
@@ -14,16 +11,12 @@ export interface DocumentHeaderProps {
 }
 
 function DocumentHeader(props: DocumentHeaderProps) {
-  const { docId, title, createdAt} = props;
-  const { id } = useParams<{ id: string }>();
-  const { data} = useQuery([id], getDocumentById);
-  console.log(data)
-  const date = format (new Date (data.createdAt),"d/MM/yyyy, HH:mm")
+  const { docId, title, createdAt } = props;
 
   return (
     <Card className="card-header p-3">
       <Card.Title>{title}</Card.Title>
-      <Card.Text className="text-muted"> {date} </Card.Text>
+      <Card.Text className="text-muted"> {createdAt} </Card.Text>
       <div className="d-flex flex-row-reverse">
         <Link to={DOCUMENT_EDIT(docId)}>
           <Button variant="secondary" size="sm" type="submit">
