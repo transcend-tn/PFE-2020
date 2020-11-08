9b122f6e3b58f3828d4f09e7e383d8538a65763d
Marwen Arfa <marwen.arfa@gmail.com>
2020-11-08 21:46:23 +0100

    fix: display  username on doc page

--- packages/client/src/components/DocumentHeader.tsx
+++ packages/client/src/components/DocumentHeader.tsx
@@ -8,15 +8,17 @@ export interface DocumentHeaderProps {
   title: string;
   createdAt: string;
   docId: string;
+  username: string;
 }
 
 function DocumentHeader(props: DocumentHeaderProps) {
-  const { docId, title, createdAt } = props;
+  const { docId, title, createdAt, username } = props;
 
   return (
     <Card className="card-header p-3">
       <Card.Title>{title}</Card.Title>
       <Card.Text className="text-muted"> {createdAt} </Card.Text>
+      <Card.Link className="text-muted"> {username} </Card.Link>
       <div className="d-flex flex-row-reverse">
         <Link to={DOCUMENT_EDIT(docId)}>
           <Button variant="secondary" size="sm" type="submit">
